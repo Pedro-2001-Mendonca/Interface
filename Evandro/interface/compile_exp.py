cdbc13724921e8b3cf3857f4b98244bdbc13a594
Evandro Alves Nakajima <47874917+evandronk@users.noreply.github.com>
2024-04-27 13:28:44 -0300

    evandro_16

--- Evandro/interface/compile_exp.py
+++ Evandro/interface/compile_exp.py
@@ -1,6 +1,6 @@
 import flet as ft
 from flet_core.matplotlib_chart import MatplotlibChart
-
+import numpy as np
 from Evandro.auxiliar import load_excel_file as excel
 from Evandro.db import db_experiment as db
 import matplotlib.pyplot as plt
@@ -15,7 +15,6 @@ def seleciona_arquivo(e: ft.FilePickerResultEvent):
             db_name = f.path
             sync_exp_list.append(db.__load_db_pack_experiment__(db_name))
         __update_page()
-        print(sync_exp_list)
 
 
 seleciona_arquivo_dialog = ft.FilePicker(on_result=seleciona_arquivo)
@@ -62,6 +61,9 @@ def __update_page():
 
     vetorCol.controls.insert(0, ft.Row(vetorLinha, alignment=ft.MainAxisAlignment.START))
 
+    list_T = []
+    list_P = []
+    list_q = []
     for i in range(len(new_list)):
         vetorLinha = [
             ft.TextField(round(new_list[i][0], 5), text_align=ft.TextAlign.CENTER, width=150,
@@ -74,9 +76,26 @@ def __update_page():
             ft.TextField(round(new_list[i][2], 5), text_align=ft.TextAlign.CENTER, width=150,
                          height=newheigth,
                          text_vertical_align=-0.73, content_padding=ft.padding.only(left=5, right=2))]
+        list_T.insert(i, new_list[i][0])
+        list_P.insert(i, new_list[i][1])
+        list_q.insert(i, new_list[i][2])
 
         vetorCol.controls.insert(i + 1, ft.Row(vetorLinha, alignment=ft.MainAxisAlignment.START))
 
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+
+    ax.scatter(list_T, list_P, list_q, c=list_q, marker="o", cmap="seismic")
+
+    ax.set_xlabel('T (K)')
+    ax.set_ylabel('P (bar)')
+    ax.set_zlabel('q (mol/kg)')
+
+    grafico = MatplotlibChart(fig.figure)
+    row_figure = ft.Row([grafico])
+    fig_container = ft.Container(content=row_figure, width=1000, height=400, )
+
     principal.controls.insert(3, vetorCol)
+    principal.controls.insert(4, fig_container)
 
     principal.update()
